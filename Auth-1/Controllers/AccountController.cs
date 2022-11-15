1602fc47ec533b37c4503c04917b5881ad192411
vijayusa11 <63645737+vijayusa11@users.noreply.github.com>
2022-11-16 01:59:33 +0530

    first commit

---
+++ Auth-1/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Auth_1.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        WebAppEntities db = new WebAppEntities();
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Login(tbl_User user)
+        {
+
+            var isUserExist = db.tbl_User.Where(x => x.UserName == user.UserName && x.Password == user.Password).Count();
+            if (isUserExist != 0)
+            {
+                FormsAuthentication.SetAuthCookie(user.UserName,false);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["msg"] = "Invalid User";
+                return View();
+            }
+            
+
+            //var isUserExist = db.tbl_User.Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
+            //if (isUserExist != null)
+            //{
+            //    return RedirectToAction("Index");
+            //}
+            //else
+            //{
+
+            //}
+            //return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return View();
+        }
+    }
+}
---
+++ Auth-1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Auth_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Auth_1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b165c15b-5c44-4ea0-8068-68263f118de3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
